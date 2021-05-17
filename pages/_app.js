e83abc8aeed2cece74a2b00a93f84fd03fa73968
Juan Sánchez <juansp92@hotmail.com>
2021-05-17 13:54:03 -0300

    chore: i18n & tailwind setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { appWithTranslation } from "next-i18next"
+import "components/data/i18n"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default appWithTranslation(MyApp)
