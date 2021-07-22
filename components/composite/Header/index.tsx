4817b97e8077281feeab050dc49ba77c1574d47a
Juan Sanchez <juansanchezplastic@gmail.com>
2021-07-22 15:00:10 -0300

    add Header component

---
+++ components/composite/Header/index.tsx
@@ -0,0 +1,42 @@
+import { useContext } from "react"
+import { useTranslation } from "react-i18next"
+import styled from "styled-components"
+import tw from "twin.macro"
+
+import { AppContext } from "components/data/AppProvider"
+import Avatar from "components/ui/Avatar"
+
+const Header: React.FC = () => {
+  const { t } = useTranslation()
+  const ctx = useContext(AppContext)
+  const email = ctx?.email as string
+  console.log(ctx)
+
+  return (
+    <HeaderContainer>
+      <Title>{t("header.title")}</Title>
+      <Wrapper className="flex">
+        <Email>{email}</Email>
+        <Avatar email={email} />
+      </Wrapper>
+    </HeaderContainer>
+  )
+}
+
+export default Header
+
+export const HeaderContainer = styled.header`
+  ${tw`flex pb-3 border-b place-content-between`}
+`
+
+export const Wrapper = styled.div`
+  ${tw`flex`}
+`
+
+export const Title = styled.h1`
+  ${tw`text-2xl font-bold invisible sm:(visible)`}
+`
+
+export const Email = styled.p`
+  ${tw`text-sm text-gray-500 mr-3 font-semibold invisible sm:(visible)`}
+`
