e85d8429359efff5db7570495125e54daab7039a
Juan Sanchez <juansanchezplastic@gmail.com>
2021-08-09 14:00:21 -0300

    Title styling

--- components/ui/Title/index.ts
+++ components/ui/Title/index.ts
@@ -2,6 +2,6 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 const Title = styled.h2`
-  ${tw`text-lg font-medium mt-2`}
+  ${tw`text-lg font-medium mt-7 mb-9 ml-5 xl:(ml-0 mt-2)`}
 `
 export default Title
