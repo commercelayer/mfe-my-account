5696b09cc90e700d0af541d67f19dbba124843bb
Juan Sanchez <juansanchezplastic@gmail.com>
2021-08-31 15:04:02 -0300

    refactor CustomerAddressCard component

--- components/ui/CustomerAddressCard/index.tsx
+++ components/ui/CustomerAddressCard/index.tsx
@@ -1,33 +1,24 @@
-import { AddressCollection } from "@commercelayer/js-sdk"
 import { Address, AddressField } from "@commercelayer/react-components"
+import { useTranslation } from "react-i18next"
+import tw from "twin.macro"
+
+import TrashIcon from "../icons/TrashIcon"
 
 interface AddressCardProps {
   addressType: "shipping" | "billing"
-  addresses?: [AddressCollection]
-  deselect: boolean
-  onSelect?: () => void
 }
 
-const CustomerAddressCard: React.FC<AddressCardProps> = ({
-  addressType,
-  addresses,
-  deselect,
-  onSelect,
-}) => {
+const CustomerAddressCard: React.FC<AddressCardProps> = ({ addressType }) => {
+  const { t } = useTranslation()
   const dataCy =
     addressType === "billing"
       ? "customer-billing-address"
       : "customer-shipping-address"
+
   return (
     <Address
       data-cy={dataCy}
-      addresses={addresses}
-      className={`${
-        onSelect && "hover:border-primary cursor-pointer"
-      } shadow-sm`}
-      selectedClassName="border-primary"
-      deselect={deselect}
-      onSelect={onSelect}
+      className="group"
       disabledClassName="opacity-50 cursor-not-allowed"
     >
       {
@@ -44,6 +35,8 @@ const CustomerAddressCard: React.FC<AddressCardProps> = ({
               countryCode={address.countryCode}
               phone={address.phone}
               addressType={addressType}
+              editButton={t("addresses.edit")}
+              deleteButton={t("addresses.delete")}
             />
           )}
         </AddressField>
@@ -65,6 +58,8 @@ interface AddressProps {
   countryCode: string
   phone: string
   addressType: string
+  editButton: string
+  deleteButton: string
 }
 
 const CustomAddress = ({
@@ -78,13 +73,15 @@ const CustomAddress = ({
   countryCode,
   phone,
   addressType,
+  editButton,
+  deleteButton,
 }: AddressProps) => (
-  <>
-    <p className="font-bold text-md" data-cy={`fullname_${addressType}`}>
+  <div tw="px-5 py-4 border rounded shadow-sm group-hover:(border-primary)">
+    <p tw="font-semibold text-md" data-cy={`fullname_${addressType}`}>
       {firstName} {lastName}
     </p>
     <p
-      className="text-sm text-gray-600"
+      tw="text-sm font-thin text-gray-500 mb-3"
       data-cy={`full_address_${addressType}`}
     >
       {[line1, line2].join(", ")}
@@ -92,6 +89,18 @@ const CustomAddress = ({
       {zipCode} {city} - {stateCode} ({countryCode})
       <br />
       {phone}
+      <br />
     </p>
-  </>
+    <div tw="flex justify-between tracking-wide text-gray-600">
+      <button tw="text-sm font-semibold h-5 cursor-pointer underline group-hover:(text-primary border-primary-light)">
+        {editButton}
+      </button>
+      <button tw="flex items-center group-hover:(text-red-400)">
+        <TrashIcon />
+        <span tw="text-sm ml-1 font-semibold cursor-pointer underline">
+          {deleteButton}
+        </span>
+      </button>
+    </div>
+  </div>
 )
