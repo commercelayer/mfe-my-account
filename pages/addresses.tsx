2a2828d60d3ace1f91c0aa0067916bbc97919459
Juan Sanchez <juansanchezplastic@gmail.com>
2021-09-29 15:24:02 +0200

    show form when user clicks on add a new customer address

--- pages/addresses.tsx
+++ pages/addresses.tsx
@@ -1,6 +1,8 @@
 import { ShippingAddressContainer } from "@commercelayer/react-components"
+import { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 
+import CustomerAddressFormNew from "components/composite/Address/CustomerAddressFormNew"
 import { AddButton } from "components/ui/AddButton"
 import CustomerAddressCard from "components/ui/CustomerAddressCard"
 import { GridContainer } from "components/ui/GridContainer"
@@ -8,14 +10,28 @@ import Title from "components/ui/Title"
 
 const Addresses = () => {
   const { t } = useTranslation()
+  const [showForm, setShowForm] = useState(false)
+
+  useEffect(() => {
+    const form = document.getElementById("customer-address-form")
+    form &&
+      (form.className =
+        "transition ease-in duration-500 transform translate-y-0 opacity-100")
+  }, [showForm])
 
   return (
     <ShippingAddressContainer>
-      <Title>{t("addresses.title")}</Title>
-      <GridContainer className="mb-6">
-        <CustomerAddressCard addressType="shipping" />
-        <AddButton action={() => {}} />
-      </GridContainer>
+      {showForm ? (
+        <CustomerAddressFormNew onClose={() => setShowForm(false)} />
+      ) : (
+        <>
+          <Title>{t("addresses.title")}</Title>
+          <GridContainer className="mb-6">
+            <CustomerAddressCard addressType="shipping" />
+            <AddButton action={() => setShowForm(true)} />
+          </GridContainer>
+        </>
+      )}
     </ShippingAddressContainer>
   )
 }
