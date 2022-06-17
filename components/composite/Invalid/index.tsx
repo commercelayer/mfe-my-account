80ea9fee9d5d62775b61b013331a0cb81485d353
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-06-17 17:43:58 +0200

    chore: edit invalid screen to show as standard 404

--- components/composite/Invalid/index.tsx
+++ components/composite/Invalid/index.tsx
@@ -1,45 +1,16 @@
-import { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
-import styled from "styled-components"
-import tw from "twin.macro"
+import { ErrorContainer } from "components/composite/ErrorContainer"
+import { ErrorCode, Text } from "components/composite/ErrorContainer/styled"
 
 const Invalid: React.FC = () => {
   const { t } = useTranslation()
-  const [title, setTitle] = useState("")
-
-  useEffect(() => {
-    window && setTitle(window.location.hostname)
-  }, [])
 
   return (
-    <Base>
-      <Container>
-        <Wrapper>
-          <Text tw="text-center text-lg font-bold pt-10 mb-10 pl-4">
-            {title}
-          </Text>
-          <div>
-            <Text data-cy="invalid-checkout" tw="py-5 h-44 text-center">
-              {t("general.invalid")}
-            </Text>
-          </div>
-        </Wrapper>
-      </Container>
-    </Base>
+    <ErrorContainer>
+      <ErrorCode>404</ErrorCode>
+      <Text data-test-id="invalid-checkout" >{t("general.invalid")}</Text>
+    </ErrorContainer>
   )
 }
-
-const Base = styled.div`
-  ${tw`bg-gray-100 min-h-screen`}
-`
-const Container = styled.div`
-  ${tw`flex items-center justify-center`}
-`
-const Wrapper = styled.div`
-  ${tw`flex-1 max-w-screen-sm	items-center justify-center`}
-`
-const Text = styled.p`
-  ${tw`py-5 h-20 text-center`}
-`
 
 export default Invalid
