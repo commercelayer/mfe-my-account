ae8d279a35c04fdf913d63d6f2544b2cfc8a64f5
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-06-21 13:58:34 +0200

    chore: load skeleton component during dynamic loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import { appWithTranslation } from "next-i18next"
 import type { AppProps } from "next/app"
 import dynamic from "next/dynamic"
 
+import MyAccountSkeleton from "components/composite/MyAccountSkeleton"
+
 import { useSettings } from "components/hooks/useSettings"
 
 const DynamicSpinnerLoader = dynamic(
@@ -13,14 +15,19 @@ const DynamicSpinnerLoader = dynamic(
 )
 const DynamicInvalid = dynamic(() => import("components/composite/Invalid"))
 const DynamicAppContainer = dynamic(
-  () => import("components/composite/MyAccountContainer")
+  () => import("components/composite/MyAccountContainer"),
+  {
+    loading: function LoadingSkeleton() {
+      const settings = {} as CustomerSettings
+      return <MyAccountSkeleton settings={settings} />
+    }
+  }
 )
 
 function UserArea(props: AppProps) {
   const { Component, pageProps } = props
-  const { settings, isLoading } = useSettings()
+  const { settings } = useSettings()
 
-  if (isLoading) return <DynamicSpinnerLoader />
   if (!settings) return <DynamicInvalid />
 
   return (
