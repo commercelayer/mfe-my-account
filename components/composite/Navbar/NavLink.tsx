2071f3a1845522f37539f9804320cb224d809e99
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-06-21 10:28:18 +0200

    chore: edit navlink to behave like in figma

--- components/composite/Navbar/NavLink.tsx
+++ components/composite/Navbar/NavLink.tsx
@@ -45,12 +45,12 @@ interface WrapperProps {
 }
 
 export const Wrapper = styled.li<WrapperProps>`
-  ${tw`flex h-14 items-center text-gray-500 hover:(cursor-pointer) active:(bg-gray-100) xl:(w-64 self-end)`}
+  ${tw`flex h-14 items-center content-center text-gray-500 hover:(cursor-pointer bg-white rounded-xl) active:(bg-gray-100) xl:(w-64 self-end)`}
   ${({ isCurrentPage }) => isCurrentPage && tw`text-black`}
 `
 
 export const Icon = styled.div`
-  ${tw`self-start pr-3 pt-2.5`}
+  ${tw`px-3`}
 `
 
 export const Wrapper2 = styled.div`
@@ -58,7 +58,7 @@ export const Wrapper2 = styled.div`
 `
 
 export const Title = styled.p`
-  ${tw`text-md font-semibold`}
+  ${tw`text-base font-semibold`}
 `
 
 export const Description = styled.p`
