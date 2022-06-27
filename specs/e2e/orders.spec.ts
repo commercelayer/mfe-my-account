8c892b4804ebac2e35e3358ed6857648516f934f
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-06-27 16:37:25 +0200

    chore: edit e2e orders spec and add addresses spec

--- specs/e2e/orders.spec.ts
+++ specs/e2e/orders.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "../fixtures/tokenizedPage"
 
-test("should land to my account home (orders page) with valid token", async ({ ordersPage }) => {
-  await ordersPage.checkOrdersTitle("My orders")
+test("should land to my account orders page with valid token", async ({ ordersPage }) => {
+  await ordersPage.checkPageTitle("My orders")
 })
