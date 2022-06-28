c2901cdb764ccbb56989580fd3c47ae9bb84d986
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-06-28 17:19:23 +0200

    chore: add NoReturns component with icon and locale

---
+++ components/composite/NoReturns/index.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+import tw from "twin.macro"
+
+import { useTranslation } from "react-i18next"
+
+import NoReturnsIcon from "components/ui/icons/NoReturnsIcon"
+import Button from "components/ui/Button"
+
+const NoReturns: React.FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <Wrapper>
+      <NoReturnsIcon />
+      <Title>{ t("noReturns.title") }</Title>
+      <Description>{ t("noReturns.description") }</Description>
+      <NoItemsButton label={ t("noReturns.buttonLabel") } />
+    </Wrapper>
+  )
+}
+
+export default NoReturns
+
+export const Wrapper = styled.div`
+  ${tw`flex flex-col items-center content-center`}
+`
+
+export const Title = styled.p`
+  ${tw`text-sm md:text-lg font-semibold mt-8`}
+`
+
+export const Description = styled.p`
+  ${tw`text-ss text-gray-500 mt-1`}
+`
+
+export const NoItemsButton = styled(Button)`
+  ${tw`mt-8`}
+`
