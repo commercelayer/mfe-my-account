c6efa65adb23e7817a196a5b54d264d8031e6695
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-06-30 16:40:41 +0200

    chore: add placeholder stuff for missing data in order detail

--- pages/orders/[orderId].tsx
+++ pages/orders/[orderId].tsx
@@ -5,8 +5,12 @@ import { useTranslation } from "react-i18next"
 import styled from "styled-components"
 import tw from "twin.macro"
 
+import ActionsMenu from "components/ui/ActionsMenu"
+import OrderTimeline from "components/composite/Order/Timeline"
 import OrderSummary from "components/composite/Order/SummarySection"
-import Title from "components/ui/Title"
+import OrderStatusChip from "components/composite/Order/OrderStatusChip"
+import Button from "components/ui/Button"
+import ActionsMenuItem from "components/ui/ActionsMenuItem"
 
 const Order: NextPage = () => {
   const router = useRouter()
@@ -17,10 +21,27 @@ const Order: NextPage = () => {
 
   return (
     <OrderContainer orderId={orderId}>
-      <Title>
-        {t("order.title")} #<OrderNumber />
-      </Title>
+      <OrderHeader>
+        <OrderHeaderMain>
+          <OrderTitle>{t("order.title")} #<OrderNumber /></OrderTitle>
+          <OrderDescription>Placed on XX/XX/XX</OrderDescription>
+          <OrderStatusChip status="placed" />
+        </OrderHeaderMain>
+        <OrderHeaderActions>
+          <MobileOnly>
+            <ActionsMenu className="mt-1">
+              <ActionsMenuItem label="Invoice" />
+              <ActionsMenuItem label="Print" />
+            </ActionsMenu>
+          </MobileOnly>
+          <DesktopOnly>
+            <Button label="Invoice" buttonSize="small" />
+            <Button label="Print" buttonSize="small" buttonStyle="outline" />
+          </DesktopOnly>
+        </OrderHeaderActions>
+      </OrderHeader>
       <Wrapper>
+        <OrderTimeline />
         <OrderSummary />
       </Wrapper>
     </OrderContainer>
@@ -29,6 +50,34 @@ const Order: NextPage = () => {
 
 export default Order
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
   ${tw`px-5 w-full md:(px-0)`}
+`
+
+const OrderHeader = styled.div`
+  ${tw`flex justify-between mt-3`}
+`
+
+const OrderHeaderMain = styled.div`
+  ${tw``}
+`
+
+const OrderTitle = styled.h2`
+  ${tw`block text-lg font-medium`}
+`
+
+const OrderDescription = styled.p`
+  ${tw`block text-sm text-gray-500 mb-2`}
+`
+
+const OrderHeaderActions = styled.div`
+  ${tw`flex justify-end`}
+`
+
+const MobileOnly = styled.div`
+  ${tw`md:(hidden)`}
+`
+
+const DesktopOnly = styled.div`
+  ${tw`hidden md:(flex gap-2 mt-2)`}
 `
