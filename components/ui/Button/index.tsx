2333e4fc94272182f9acd218b27752a4990c98ff
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-06-30 10:20:11 +0200

    chore: clean Button and add buttonSize + buttonStyle props

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -4,12 +4,13 @@ import tw from "twin.macro"
 type PrimaryButtonProps = {
   label?: string
   onClick?: () => void
-  addressId?: string
+  buttonSize?: string
+  buttonStyle?: string
 }
 
 const Button: React.FC<PrimaryButtonProps> = ( props ) => {
   const {
-    label = 'Continue to delivery',
+    label = 'Click me',
     onClick,
     ...p
   } = props
@@ -31,6 +32,8 @@ const Button: React.FC<PrimaryButtonProps> = ( props ) => {
 
 export default Button
 
-const PrimaryButton = styled.button`
-  ${tw`text-ss uppercase text-white bg-primary text-center px-12 h-11 rounded-md shadow-sm disabled:opacity-50 hover:opacity-80 transition duration-500 ease-in-out`}
+const PrimaryButton = styled.button<PrimaryButtonProps>`
+  ${tw` uppercase text-center h-11 rounded-md shadow-sm disabled:opacity-50 hover:opacity-80 transition duration-500 ease-in-out`}
+  ${({ buttonSize }) => (buttonSize == 'small' ? tw`px-5 h-5 text-3xs` : tw`px-12 h-11 text-ss`)}
+  ${({ buttonStyle }) => (buttonStyle == 'outline' ? tw`text-gray-600 bg-gray-200 border border-gray-350` : tw`text-white bg-primary`)}
 `
