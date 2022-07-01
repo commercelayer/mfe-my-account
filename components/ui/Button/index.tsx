11d3e0990b29abe8977601b8d4b57ec065bdf404
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-01 12:10:54 +0200

    chore: edit Button to remove default uppercase and fix side padding

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -33,7 +33,7 @@ const Button: React.FC<PrimaryButtonProps> = ( props ) => {
 export default Button
 
 const PrimaryButton = styled.button<PrimaryButtonProps>`
-  ${tw` uppercase text-center h-11 rounded-md shadow-sm disabled:opacity-50 hover:opacity-80 transition duration-500 ease-in-out`}
-  ${({ buttonSize }) => (buttonSize == 'small' ? tw`px-5 h-5 text-3xs` : tw`px-12 h-11 text-ss`)}
+  ${tw` text-center h-11 rounded-md shadow-sm disabled:opacity-50 hover:opacity-80 transition duration-500 ease-in-out`}
+  ${({ buttonSize }) => (buttonSize == 'small' ? tw`px-5 h-5 text-3xs` : tw`px-7 h-11 text-ss`)}
   ${({ buttonStyle }) => (buttonStyle == 'outline' ? tw`text-gray-600 bg-gray-200 border border-gray-350` : tw`text-white bg-primary`)}
 `
