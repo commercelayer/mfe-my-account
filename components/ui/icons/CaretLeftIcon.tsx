65e1bf69959387c705b052a9fe93423ca524aa5a
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-11 12:25:00 +0200

    chore: add CaretLeftIcon ui icon

---
+++ components/ui/icons/CaretLeftIcon.tsx
@@ -0,0 +1,9 @@
+const CaretLeftIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-7 h-7" fill="none" >
+      <path d="M15 19.5L7.5 12L15 4.5" stroke="#101111" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  )
+}
+
+export default CaretLeftIcon
