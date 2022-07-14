682bc5979f6e892264924a3677d69defcfac52e5
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-14 15:24:08 +0200

    chore: edit SummarySection styled to add top margin

--- components/composite/Order/SummarySection/styled.tsx
+++ components/composite/Order/SummarySection/styled.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 export const Wrapper = styled.div`
-  ${tw``}
+  ${tw` mt-12`}
 `
 export const SummaryWrapper = styled.div`
   ${tw``}
