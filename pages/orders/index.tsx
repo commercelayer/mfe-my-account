1b82a8b25be765992c3436290c02df3329e0ee9c
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-18 16:54:27 +0200

    chore: clean OrdersPage from unwanted code

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from "next"
-import { useRouter } from "next/router"
 
 import Orders from "components/composite/Orders"
 import EmptyOrders from "components/composite/Orders/EmptyOrders"
@@ -8,14 +7,11 @@ interface Props {
   settings: CustomerSettings
 }
 
-const OrderPage: NextPage<Props> = ({ settings }) => {
-  const { query } = useRouter()
-  const orderId = query.orderId as string
-
+const OrdersPage: NextPage<Props> = ({ settings }) => {
   // return ( <EmptyOrders settings={settings} /> )
   return (
     <Orders settings={settings} />
   )
 }
 
-export default OrderPage
+export default OrdersPage
