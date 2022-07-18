9bd00bde38aac60246d0323a1506b8696d782a74
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-18 16:19:36 +0200

    chore: add centered prop to UI Card to remove right margins

--- components/ui/Card/index.tsx
+++ components/ui/Card/index.tsx
@@ -5,6 +5,7 @@ interface Props {
   className?: string
   rounded?: boolean
   fullHeight?: boolean
+  centered?: boolean
 }
 
 export const Card: React.FC<Props> = ({
@@ -12,8 +13,9 @@ export const Card: React.FC<Props> = ({
   className,
   rounded,
   fullHeight,
+  centered
 }) => (
-  <Wrapper className={className} rounded={rounded} fullHeight={fullHeight}>
+  <Wrapper className={className} rounded={rounded} fullHeight={fullHeight} centered={centered}>
     {children}
   </Wrapper>
 )
@@ -21,10 +23,12 @@ export const Card: React.FC<Props> = ({
 interface WrapperProps {
   rounded?: boolean
   fullHeight?: boolean
+  centered?: boolean
 }
 
 const Wrapper = styled.div<WrapperProps>`
-  ${tw`flex flex-col min-h-full p-5 pt-0 md:p-5 lg:p-10 lg:pr-20 xl:pr-48 bg-gray-100 bg-gray-100 md:bg-white shadow-sm`}
+  ${tw`flex flex-col min-h-full p-5 pt-0 md:p-5 lg:p-10 bg-gray-100 bg-gray-100 md:bg-white shadow-sm`}
   ${({ rounded }) => (rounded ? tw`rounded-md` : null)}
   ${({ fullHeight }) => (fullHeight ? tw`min-h-full ` : null)}
+  ${({ centered }) => (centered ? null : tw` lg:pr-20 xl:pr-48 ` )}
 `
