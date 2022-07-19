a7714dc597b957c45ef7d9f80309e403ce7af117
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-19 16:36:18 +0200

    chore: edit _app page to use new Settings provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,47 +4,41 @@ import type { AppProps } from "next/app"
 import "components/data/i18n"
 import { useRouter } from "next/router"
 
-import { RetryError } from "components/composite/RetryError"
-import { useSettingsOrInvalid } from "components/hooks/useSettingsOrInvalid"
-
+import MyAccountContainer from "components/composite/MyAccountContainer"
+import Invalid from "components/composite/Invalid"
 import MyAccountSkeleton from "components/composite/MyAccountSkeleton"
-import dynamic from "next/dynamic"
+import MyAccountSkeletonGuest from "components/composite/MyAccountSkeleton/Guest"
+
+import { isGuest } from "utils/isGuest"
+
+import { SettingsProvider } from "components/data/SettingsProvider"
 
 function MyAccount(props: AppProps) {
   const { Component, pageProps } = props
-
-  const DynamicInvalid = dynamic(() => import("components/composite/Invalid"))
-
-  const DynamicAppContainer = dynamic(
-    () => import("components/composite/MyAccountContainer"),
-    {
-      loading: function LoadingSkeleton() {
-        return <MyAccountSkeleton />
-      }
-    }
-  )
-
   const router = useRouter()
-  const { settings, retryOnError, isLoading } = useSettingsOrInvalid()
-  if (router.pathname != '/404') {
-    if (isLoading) return <MyAccountSkeleton />
-    if (!isLoading && !settings) return <DynamicInvalid />
-    if (!settings || retryOnError) return <RetryError />
+  const orderId = router.query.orderId as string
 
-    /** If you are a Guest user you can view just the order detail and children pages */
-    if (settings && settings.isGuest && router.pathname.indexOf('/orders/[orderId]') == -1) {
-      router.push(`/404?accessToken=${settings.accessToken}`)
-    }
-
+  if (router.pathname !== '/404') {
     return (
-      <DynamicAppContainer settings={settings}>
-        <Component {...pageProps} settings={settings} />
-      </DynamicAppContainer>
+      <SettingsProvider orderId={orderId}>
+        {({ settings, isLoading }) => {
+          return isLoading && isGuest() ? (
+            <MyAccountSkeletonGuest />
+          ) : isLoading && !isGuest() ? ( 
+            <MyAccountSkeleton />
+          ) : !settings.validUserArea ? (
+            <Invalid />
+          ) : (
+            <MyAccountContainer settings={settings}>
+              <Component {...pageProps} settings={settings} />
+            </MyAccountContainer>
+          )
+        }}
+      </SettingsProvider>
     )
   } else {
     return <Component {...pageProps} />
   }
-  
   
 }
 
