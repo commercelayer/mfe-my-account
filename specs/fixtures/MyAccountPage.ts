09961f7e8986729a8f99444da6d208e14053dbe4
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-20 14:44:02 +0200

    chore: remove api/settings request from playwright tests

--- specs/fixtures/MyAccountPage.ts
+++ specs/fixtures/MyAccountPage.ts
@@ -1,7 +1,7 @@
 import { Page, expect } from "@playwright/test"
 
 interface GoToProps {
-  pageUrl: string,
+  pageUrl: string
   token: string
 }
 
@@ -21,24 +21,6 @@ export class MyAccountPage {
 
   async goto({ pageUrl, token }: GoToProps) {
     const url = `${pageUrl}?accessToken=${token}`
-
-    await this.page.route("**/api/settings**", async (route) => {
-      // Fetch original response.
-      const response = await this.page.request.fetch(route.request())
-
-      // // Add a prefix to the title.
-      const body = await response.json()
-      // // body = body.replace('<title>', '<title>My prefix:');
-      route.fulfill({
-        // Pass all fields from the response.
-        response,
-        // Override response body.
-        body: JSON.stringify({
-          ...body,
-          ...this.attributes?.organization,
-        }),
-      })
-    })
 
     await this.page.goto(`${url}`, {
       waitUntil: "networkidle",
