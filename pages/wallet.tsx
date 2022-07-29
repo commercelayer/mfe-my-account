be5eb6c072581de23df60c72d697d2414706987a
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-07-29 12:09:31 +0200

    chore: Add wallet page to test his empty state and translations

--- pages/wallet.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next"
-
-const Wallet: NextPage = () => {
-  return <h1>Wallet Page</h1>
-}
-
-export default Wallet
---
+++ pages/wallet/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+import Empty from "components/composite/Empty"
+
+const WalletPage: NextPage = () => {
+  return <Empty type="PaymentMethods" />
+}
+
+export default WalletPage
