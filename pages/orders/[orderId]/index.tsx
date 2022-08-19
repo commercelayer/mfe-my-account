7baf13521a7a3dfedcf9f5a7ce8bd3e50719ca9e
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-08-19 17:14:53 +0200

    chore: Add use of OrderProvider in OrderPage

--- pages/orders/[orderId]/index.tsx
+++ pages/orders/[orderId]/index.tsx
@@ -2,12 +2,20 @@ import { NextPage } from "next"
 import { useRouter } from "next/router"
 
 import Order from "components/composite/Order"
+import { OrderProvider } from "components/data/OrderProvider"
 
 const OrderPage: NextPage = () => {
   const { query } = useRouter()
   const orderId = query.orderId as string
+  const accessToken = query.accessToken as string
 
-  return <Order orderId={orderId} />
+  return (
+    <OrderProvider orderId={orderId} accessToken={accessToken}>
+      {({ order }) => {
+        return <Order orderId={orderId} order={order} />
+      }}
+    </OrderProvider>
+  )
 }
 
 export default OrderPage
