f28b4e4140f90c34c75569c259523b458826b0cb
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-08-22 16:50:17 +0200

    chore: Add refines to ActionsMenuItem styles to manage with/without icon case correctly

--- components/ui/ActionsMenuItem/styled.tsx
+++ components/ui/ActionsMenuItem/styled.tsx
@@ -4,17 +4,17 @@ import tw from "twin.macro"
 import { LabelProps } from "./"
 
 export const Wrapper = styled.div`
-  ${tw`flex items-center py-4 bg-white hover:bg-gray-200`}
+  ${tw`flex items-center px-2 py-4 bg-white hover:bg-gray-200 cursor-default`}
 `
 
 export const IconWrapper = styled.div`
-  ${tw`flex items-center`}
+  ${tw`flex items-center px-2`}
 `
 
 export const Label = styled.p<LabelProps>`
-  ${tw`text-3xs`}
+  ${tw`text-[12px] select-none`}
   ${({ icon }) => {
-    return !icon && tw`ml-5`
+    return !icon && tw`ml-7`
   }}
   ${({ variant }) => {
     return variant === "warning" && tw`text-red-400`
