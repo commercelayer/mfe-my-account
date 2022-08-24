05bd376454eaee38a2c4db284a52050f3a5f40b4
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-08-24 12:23:07 +0200

    chore: Fix Button small styles

--- components/ui/Button/styled.tsx
+++ components/ui/Button/styled.tsx
@@ -4,9 +4,9 @@ import tw from "twin.macro"
 import { PrimaryButtonProps } from "."
 
 export const PrimaryButton = styled.button<PrimaryButtonProps>`
-  ${tw` text-center h-11 rounded-md shadow-sm disabled:opacity-50 hover:opacity-80 transition duration-500 ease-in-out`}
+  ${tw` text-center font-semibold h-11 rounded-md shadow-sm disabled:opacity-50 hover:opacity-80 transition duration-500 ease-in-out`}
   ${({ buttonSize }) =>
-    buttonSize === "small" ? tw`px-5 h-5 text-3xs` : tw`px-7 h-11 text-ss`}
+    buttonSize === "small" ? tw`px-5 h-6 text-3xs` : tw`px-7 h-11 text-ss`}
   ${({ buttonStyle }) =>
     buttonStyle === "outline"
       ? tw`text-gray-600 bg-gray-200 border border-gray-350`
