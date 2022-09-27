c3abda770a175529f7ad989942fde5b9b17c620d
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-09-27 16:59:49 +0200

    chore: add PaymentMethod component in OrderPayments

--- components/composite/Order/OrderPayments/index.tsx
+++ components/composite/Order/OrderPayments/index.tsx
@@ -20,25 +20,27 @@ const OrderPayments: React.FC<Props> = ({ order }) => {
 
   return (
     <PaymentMethodsContainer>
-      <PaymentSource readonly>
-        <PaymentSourceBrandIcon className="mr-2" />
-        <PaymentSourceBrandName className="mr-1" />
-        {/* {({ brand }) => {
-              if ((order?.payment_source?.type as string) === "credit_card") {
-                return (
-                  <Trans i18nKey="order.paymentMethod.EndingIn">
-                    {brand}
-                    <PaymentSourceDetail
-                      className="ml-1 font-normal"
-                      type="last4"
-                    />
-                  </Trans>
-                )
-              }
-              return getTranslations(brand, t)
-            }}
-          </PaymentSourceBrandName> */}
-      </PaymentSource>
+      <PaymentMethod>
+        <PaymentSource readonly>
+          <PaymentSourceBrandIcon className="mr-2" />
+          <PaymentSourceBrandName className="mr-1" />
+          {/* {({ brand }) => {
+                if ((order?.payment_source?.type as string) === "credit_card") {
+                  return (
+                    <Trans i18nKey="order.paymentMethod.EndingIn">
+                      {brand}
+                      <PaymentSourceDetail
+                        className="ml-1 font-normal"
+                        type="last4"
+                      />
+                    </Trans>
+                  )
+                }
+                return getTranslations(brand, t)
+              }}
+            </PaymentSourceBrandName> */}
+        </PaymentSource>
+      </PaymentMethod>
     </PaymentMethodsContainer>
   )
 }
