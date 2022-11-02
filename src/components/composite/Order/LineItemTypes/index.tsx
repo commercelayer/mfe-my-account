c103c31aa15fcfd9e8132b61fbf8826c3810ae33
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-11-02 15:40:43 +0100

    fix: fix order summary lineItems

--- src/components/composite/Order/LineItemTypes/index.tsx
+++ src/components/composite/Order/LineItemTypes/index.tsx
@@ -2,13 +2,20 @@ import {
   LineItem,
   LineItemImage,
   LineItemName,
+  LineItemCode,
   LineItemQuantity,
   LineItemAmount,
 } from "@commercelayer/react-components"
 import type { LineItemType } from "@commercelayer/react-components"
 import { useTranslation } from "react-i18next"
 
-import { LineItemWrapper, LineItemDescription, LineItemQty } from "./styled"
+import {
+  LineItemWrapper,
+  LineItemContent,
+  LineItemDescription,
+  LineItemSku,
+  LineItemQty,
+} from "./styled"
 
 type Props = {
   type: LineItemType
@@ -21,20 +28,25 @@ export const LineItemTypes: React.FC<Props> = ({ type }) => {
     <LineItem type={type}>
       <LineItemWrapper>
         <LineItemImage className="self-start p-1 border rounded w-[75px] md:w-[85px] bg-contrast" />
-        <LineItemDescription>
-          <LineItemName className="font-bold" />
-          <LineItemQty>
-            <LineItemQuantity>
-              {(props) => (
-                <>
-                  {!!props.quantity &&
-                    t("order.quantity", { count: props.quantity })}
-                </>
-              )}
-            </LineItemQuantity>
-          </LineItemQty>
-          <LineItemAmount className="mt-2 font-extrabold" />
-        </LineItemDescription>
+        <LineItemContent>
+          <LineItemDescription>
+            <LineItemSku>
+              SKU <LineItemCode className="text-xs text-gray-600" />
+            </LineItemSku>
+            <LineItemName className="block mb-1 font-bold" />
+            <LineItemQty>
+              <LineItemQuantity>
+                {(props) => (
+                  <>
+                    {!!props.quantity &&
+                      t("order.quantity", { count: props.quantity })}
+                  </>
+                )}
+              </LineItemQuantity>
+            </LineItemQty>
+          </LineItemDescription>
+          <LineItemAmount className="pt-4 text-lg font-extrabold" />
+        </LineItemContent>
       </LineItemWrapper>
     </LineItem>
   )
--- src/components/composite/Order/LineItemTypes/styled.tsx
+++ src/components/composite/Order/LineItemTypes/styled.tsx
@@ -2,11 +2,17 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 export const LineItemWrapper = styled.div`
-  ${tw`flex flex-row pt-6 pb-8 border-b`}
+  ${tw`flex flex-row w-full pt-6 pb-8 border-b`}
+`
+export const LineItemContent = styled.div`
+  ${tw`flex justify-between w-full pl-4 md:pl-8`}
 `
 export const LineItemDescription = styled.div`
-  ${tw`pl-4 flex flex-col flex-1 md:pl-8`}
+  ${tw``}
 `
-export const LineItemQty = styled.div`
-  ${tw`text-xs uppercase mt-1 text-gray-500`}
+export const LineItemSku = styled.div`
+  ${tw`flex gap-1 text-xs text-gray-600 font-semibold h-4`}
+`
+export const LineItemQty = styled.span`
+  ${tw`text-xs font-semibold uppercase bg-gray-300 text-gray-600 py-[4px] px-[12px] rounded-full`}
 `
