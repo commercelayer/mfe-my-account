79fcc169b2254e999bb1dcb3b20100b1ad499c0b
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-11-04 12:29:56 +0100

    fix: move mobile fixed menu spacings to right component

--- src/components/ui/PageMain/styled.tsx
+++ src/components/ui/PageMain/styled.tsx
@@ -2,5 +2,5 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 export const Wrapper = styled.div`
-  ${tw`flex-1 pb-16 lg:pb-0`}
+  ${tw`flex-1 pb-16 lg:pb-0 pt-[114px] lg:pt-0`}
 `
