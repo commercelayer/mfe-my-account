64b0a57823f50f8a45fc73f6d7a838a682fdf052
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-11-11 12:25:57 +0100

    chore: rebalance skeleton order list columns and fix margins

--- src/components/composite/Skeleton/Main/OrdersTable/index.tsx
+++ src/components/composite/Skeleton/Main/OrdersTable/index.tsx
@@ -4,7 +4,6 @@ import {
   SkeletonTableTHead,
   SkeletonCol,
   SkeletonSpan,
-  SkeletonSubtitle,
 } from "src/components/composite/Skeleton/styled"
 
 type Props = {
@@ -13,19 +12,19 @@ type Props = {
 
 const SkeletonMainOrdersTableRow: React.FC = () => {
   return (
-    <SkeletonTableRow className="relative items-center pb-12 h-[107px] lg:h-[96px] md:pb-0">
+    <SkeletonTableRow className="relative items-center pb-12 mb-4 lg:mb-0 h-[107px] lg:h-[96px] md:pb-0">
       <SkeletonCol className="order-1 w-1/4 pt-6 pb-2.5 md:p-0 md:align-middle">
         <SkeletonSpan size="small" />
-        <SkeletonSpan size="small" />
+        <SkeletonSpan size="medium" />
       </SkeletonCol>
       <SkeletonCol className="absolute order-2 w-1/4 bottom-4 md:bottom-auto md:relative md:text-left">
         <SkeletonSpan size="small" />
       </SkeletonCol>
       <SkeletonCol className="absolute right-0 order-3 w-1/4 bottom-4 md:bottom-auto md:relative">
-        <SkeletonSpan size="small" className="self-end md:self-auto" />
+        <SkeletonSpan size="medium" className="self-end md:self-auto" />
       </SkeletonCol>
       <SkeletonCol className="order-4 w-1/4 font-bold text-right md:text-left md:text-lg">
-        <SkeletonSubtitle size="small" className="self-end md:self-auto" />
+        <SkeletonSpan size="small" className="self-end md:self-auto" />
       </SkeletonCol>
     </SkeletonTableRow>
   )
