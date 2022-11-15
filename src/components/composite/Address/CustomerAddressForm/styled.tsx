b65df9e24124c382739d80a03d8817a4529e246e
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-11-15 16:13:13 +0100

    fix: fix address form discard changes action to be on single line

--- src/components/composite/Address/CustomerAddressForm/styled.tsx
+++ src/components/composite/Address/CustomerAddressForm/styled.tsx
@@ -27,13 +27,13 @@ export const FormButtons = styled.div`
 `
 
 export const DiscardChanges = styled.div<DiscardChangesProps>`
-  ${tw`flex items-center text-ss text-primary underline border-red-400 font-bold hover:(cursor-pointer)`}
+  ${tw`flex content-center items-center text-ss text-primary underline border-red-400 font-bold hover:(cursor-pointer)`}
 `
 
 export const SaveButton = styled(SaveAddressesButton)`
-  ${tw`text-ss font-bold text-white bg-primary text-center px-20 h-11 rounded-md shadow-sm disabled:opacity-50`}
+  ${tw`text-ss font-bold text-white bg-primary text-center px-16 lg:px-20 h-11 rounded-md shadow-sm disabled:opacity-50 max-w-1/2`}
 `
 
 export const Text = styled.p`
-  ${tw`ml-1.5 border-b border-green-400 border-gray-200/10 w-min md:w-auto`}
+  ${tw`ml-1.5 border-b border-green-400 border-gray-200/10`}
 `
