25c731d8c3189f56532e1e33199620f6cb7fb466
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-12-09 19:17:54 +0100

    fix: fix some imports orderings

--- src/components/composite/MyAccountContainer/index.tsx
+++ src/components/composite/MyAccountContainer/index.tsx
@@ -1,18 +1,17 @@
 import { CommerceLayer } from "@commercelayer/react-components/auth/CommerceLayer"
 import { GlobalStylesProvider } from "@commercelayer/react-utils"
-import { GlobalStyles as BaseStyles } from 'twin.macro'
 import type { Settings } from "HostedApp"
 import { IconContext } from "phosphor-react"
+import { GlobalStyles as BaseStyles } from "twin.macro"
 
 import CustomerHeader from "#components/composite/Header/Customer"
 import GuestHeader from "#components/composite/Header/Guest"
-import { PageHead } from "#components/composite/PageHead"
 import Navbar from "#components/composite/Navbar"
+import { PageHead } from "#components/composite/PageHead"
 import { LayoutDefault } from "#components/layouts/LayoutDefault"
 import { FooterWrapper } from "#components/ui/Common/styled"
 import Footer from "#components/ui/Footer"
 import PageMain from "#components/ui/PageMain"
-
 import { AppProvider } from "#providers/AppProvider"
 import { CustomerContainerProvider } from "#providers/CustomerContainerProvider"
 
