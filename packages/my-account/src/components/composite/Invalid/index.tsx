b14b4ac1dd4a8bbe9dab1a967b8ad67ab117c3d5
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2022-12-13 14:48:34 +0100

    chore: introduce code and message by props in Invalid component

--- packages/my-account/src/components/composite/Invalid/index.tsx
+++ packages/my-account/src/components/composite/Invalid/index.tsx
@@ -3,13 +3,20 @@ import { useTranslation } from "react-i18next"
 import { ErrorContainer } from "#components/composite/ErrorContainer"
 import { ErrorCode, Text } from "#components/composite/ErrorContainer/styled"
 
-function Invalid(): JSX.Element {
+interface Props {
+  statusCode?: string
+  message?: string
+}
+
+function Invalid(props: Props): JSX.Element {
   const { t } = useTranslation()
+
+  const { statusCode = "404", message = t("general.invalid") } = props
 
   return (
     <ErrorContainer>
-      <ErrorCode>404</ErrorCode>
-      <Text data-test-id="invalid-checkout">{t("general.invalid")}</Text>
+      <ErrorCode>{statusCode}</ErrorCode>
+      <Text>{message}</Text>
     </ErrorContainer>
   )
 }
