6c64c831e5e41974cd70862a77c61b443e3d80d9
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-03-08 11:56:38 +0100

    chore: add white bg to grid card element

--- packages/my-account/src/components/ui/GridCard/styled.tsx
+++ packages/my-account/src/components/ui/GridCard/styled.tsx
@@ -8,7 +8,7 @@ interface WrapperProps {
 }
 
 export const Wrapper = styled.div<WrapperProps>`
-  ${tw`rounded-[5px] border border-gray-200 p-[1px]`}
+  ${tw`rounded-[5px] border border-gray-200 p-[1px] bg-white`}
   ${({ hover }) =>
     hover === undefined &&
     tw`hover:(border-primary border-2 shadow-sm-primary p-0)`}
