cef1b392479902770a43acac9b39a78c5963b9be
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-04-09 13:04:21 +0200

    feat: add line item frequency and refine qty box ui

--- packages/my-account/src/components/ui/LineItem/styled.tsx
+++ packages/my-account/src/components/ui/LineItem/styled.tsx
@@ -2,17 +2,20 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 export const LineItemWrapper = styled.div`
-  ${tw`flex flex-row w-full pb-6 not-last-of-type:border-b`}
+  ${tw`relative flex flex-row w-full mb-6 not-last-of-type:border-b`}
 `
 export const LineItemContent = styled.div`
   ${tw`flex justify-between w-full pl-4 md:pl-8`}
 `
 export const LineItemDescription = styled.div`
-  ${tw``}
+  ${tw`flex flex-col justify-between`}
 `
 export const LineItemSku = styled.div`
   ${tw`flex gap-1 text-xs text-gray-600 font-semibold h-4`}
 `
 export const LineItemQty = styled.span`
-  ${tw`text-xs font-semibold uppercase bg-gray-300 text-gray-600 py-[4px] px-[12px] rounded-full`}
+  ${tw`self-start w-auto text-xs font-semibold ring ring-inset ring-1 ring-gray-100 bg-gray-100 text-gray-600 py-[4px] px-[10px] rounded`}
+`
+export const LineItemFrequency = styled(LineItemQty)`
+  ${tw`mt-2 flex bg-white ring-primary text-primary lg:mt-0`}
 `
