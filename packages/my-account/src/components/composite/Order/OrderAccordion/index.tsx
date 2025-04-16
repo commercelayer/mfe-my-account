98bc75d22181cd1db3135f209cb7f86a62631d65
TkTioNG <tktiong@icloud.com>
2025-04-16 17:36:49 +0800

    [MT-584] Remove shipments section

--- packages/my-account/src/components/composite/Order/OrderAccordion/index.tsx
+++ packages/my-account/src/components/composite/Order/OrderAccordion/index.tsx
@@ -1,4 +1,3 @@
-import { useContext } from "react"
 import { useTranslation } from "react-i18next"
 
 import { Wrapper, SummaryWrapper } from "./styled"
@@ -6,14 +5,11 @@ import { Wrapper, SummaryWrapper } from "./styled"
 import AddressesSummary from "#components/composite/Order/AddressesSummary"
 import LineItemList from "#components/composite/Order/LineItemList"
 import OrderPayments from "#components/composite/Order/OrderPayments"
-import OrderShipments from "#components/composite/Order/OrderShipments"
 import OrderSummary from "#components/composite/Order/OrderSummary"
 import { OrderSection, OrderSectionItem } from "#components/ui/OrderSection"
-import { OrderContext } from "#providers/OrderProvider"
 
 function OrderSections(): JSX.Element {
   const { t } = useTranslation()
-  const orderCtx = useContext(OrderContext)
 
   return (
     <Wrapper>
@@ -33,14 +29,6 @@ function OrderSections(): JSX.Element {
         >
           <AddressesSummary />
         </OrderSectionItem>
-        {(orderCtx?.order?.shipments ?? []).length > 0 && (
-          <OrderSectionItem
-            index={3}
-            header={<span>{t("order.shipments.title")}</span>}
-          >
-            <OrderShipments />
-          </OrderSectionItem>
-        )}
         <OrderSectionItem
           index={4}
           header={<span>{t("order.payments.title")}</span>}
