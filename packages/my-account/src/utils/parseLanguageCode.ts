137b3444e4fcc78d69c8472d532ec79d1091ab50
quanticdreams7 <quanticdreams7@gmail.com>
2025-05-14 15:51:50 +0200

    add nl lang to parseLanguageCode.ts and react-i18next.d.ts

--- packages/my-account/src/utils/parseLanguageCode.ts
+++ packages/my-account/src/utils/parseLanguageCode.ts
@@ -1,10 +1,11 @@
 import type { AllowedLocaleKeys } from "react-i18next"
 
-type ApiLanguageCode = "en" | "it"
+type ApiLanguageCode = "en" | "it" | "nl"
 
 const langs: Record<ApiLanguageCode, AllowedLocaleKeys> = {
   en: "en",
   it: "it",
+  nl: "nl",
 }
 
 export const parseLanguageCode = (apiLanguageCode: string) =>
