e5b54ff00076a2ee697d86de2aa5cfa18a9d9394
quanticdreams7 <quanticdreams7@gmail.com>
2025-05-14 12:06:58 +0200

    Add dutch (NL) locale to mfe-my-account

--- packages/my-account/src/utils/i18n.ts
+++ packages/my-account/src/utils/i18n.ts
@@ -3,8 +3,9 @@ import { initReactI18next } from "react-i18next"
 
 import translationEN from "#assets/locales/en/common.json"
 import translationIT from "#assets/locales/it/common.json"
+import translationNL from "#assets/locales/nl/common.json"
 
-const languages = ["en", "it"]
+const languages = ["en", "it", "nl"]
 
 const resources = {
   en: {
@@ -12,6 +13,9 @@ const resources = {
   },
   it: {
     translation: translationIT,
+  },
+  nl: {
+    translation: translationNL,
   },
 }
 
