5abf7aad521ff9701128d05631950ba53f3778c2
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2025-12-15 17:17:58 +0100

    feat: add delete button in wallet cards

--- packages/my-account/src/components/composite/PaymentSourceCard.tsx
+++ packages/my-account/src/components/composite/PaymentSourceCard.tsx
@@ -1,38 +1,82 @@
 import { PaymentSourceBrandIcon } from "@commercelayer/react-components/payment_source/PaymentSourceBrandIcon"
 import { PaymentSourceDetail } from "@commercelayer/react-components/payment_source/PaymentSourceDetail"
+import { useState } from "react"
+import { useTranslation } from "react-i18next"
+import { Trash, X } from "phosphor-react"
+import { LinkButton } from "#components/ui/LinkButton"
 
 import {
   PaymentSourceName,
   PaymentSourceCreditCardNumber,
   PaymentSourceCreditCardExpires,
 } from "#components/composite/PaymentSource"
+import { GridCard } from "#components/ui/GridCard"
+import { PaymentSourceDeleteButton } from "@commercelayer/react-components"
 
 export function PaymentSourceCard(): JSX.Element {
+  const { t } = useTranslation()
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false)
+
   return (
-    <div className="rounded flex flex-col gap-6">
-      <div>
-        <PaymentSourceBrandIcon width={50} />
-      </div>
-      <div className="flex flex-col w-full text-sm">
-        <PaymentSourceDetail type="last4">
-          {(props) => {
-            if (props.text === null || props.text.length === 0)
-              return (
-                <div className="flex gap-1">
-                  <PaymentSourceName />
+    <div className="relative transition duration-200 ease-in bg-white md:bg-transparent focus:shadow-sm">
+      {showDeleteConfirmation && (
+        <div className="absolute top-0 left-0 right-0 flex flex-col items-center justify-center h-full p-5 text-center bg-white border-2 border-red-400 rounded">
+          <X
+            weight="regular"
+            className="absolute w-5 h-5 text-gray-300 cursor-pointer right-2 top-2"
+            onClick={() => setShowDeleteConfirmation(false)}
+          />
+          <p className="text-sm font-bold">{t("wallet.deleteConfirmation")}</p>
+          <div className="flex justify-center w-full mt-3 px-5">
+            <PaymentSourceDeleteButton
+              label={t("paymentSource.delete")}
+              className="address-confirm-delete-button form-button px-5 h-6 text-3xs rounded-md bg-red-400 text-white flex items-center justify-center mx-0.5"  
+            />
+          </div>
+        </div>
+      )}
+      <GridCard>
+        <div className="flex flex-col gap-6">
+          <div>
+            <PaymentSourceBrandIcon width={50} />
+          </div>
+          <div className="flex flex-col w-full text-sm">
+            <PaymentSourceDetail type="last4">
+              {(props) => {
+                if (props.text === null || props.text.length === 0)
+                  return (
+                    <div className="flex gap-1">
+                      <PaymentSourceName />
+                    </div>
+                  )
+                return (
+                  <>
+                    <div className="flex gap-1">
+                      <PaymentSourceCreditCardNumber />
+                    </div>
+                    <PaymentSourceCreditCardExpires variant="card" />
+                  </>
+                )
+              }}
+            </PaymentSourceDetail>
+            <div className="flex flex-col justify-end pt-2">
+              <div className="flex justify-between self-end tracking-wide">
+                <div
+                  onClick={() => setShowDeleteConfirmation(true)}
+                  className="address-delete-button flex items-center gap-1 text-gray-400 group-hover:text-red-400"
+                >
+                  <Trash className="w-3.5 h-3.5" />
+                  <LinkButton
+                    className="form-button group-hover:text-red-400 bg-white"
+                    label={t("paymentSource.delete")}
+                    variant="warning"
+                  />
                 </div>
-              )
-            return (
-              <>
-                <div className="flex gap-1">
-                  <PaymentSourceCreditCardNumber />
-                </div>
-                <PaymentSourceCreditCardExpires variant="card" />
-              </>
-            )
-          }}
-        </PaymentSourceDetail>
-      </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </GridCard>
     </div>
   )
 }
--- packages/my-account/src/components/ui/GridCard.tsx
+++ packages/my-account/src/components/ui/GridCard.tsx
@@ -11,7 +11,7 @@ export function GridCard(props: Props): JSX.Element {
   const { children, hover } = props
 
   return (
-    <div className={cn('rounded-[5px] border border-gray-200 p-[1px] bg-white', {
+    <div className={cn('group rounded-[5px] border border-gray-200 p-[1px] bg-white', {
       "hover:border-primary hover:border-2 hover:shadow-sm-primary hover:p-0": hover === undefined
     })}>
       <div className="p-4">{children}</div>
